da1cb3d0ff086980f101250e700b0d2f4885dd5d
Swapnaneel <swapnaneel.log.in.16@gmail.com>
2026-05-20 21:49:17 -0400

    Resume Circle and Resume Home Fitting

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,18 +15,18 @@ export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback</h2>
       </div>
-    </section>
-    {resumes.length > 0 &&(
-        <div>
-          {resumes.map((resume)=>(
-              <ResumeCard key={resume.id} resume = {resume}  />
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
           ))}
         </div>
-    )}
+      )}
+    </section>
 
   </main>
 }
